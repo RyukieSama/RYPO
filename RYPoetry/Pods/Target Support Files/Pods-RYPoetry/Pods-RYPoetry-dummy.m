499c5c1eb39cf154c3659c5e6e8e3bccf2f4d202
RyukieSama <ryukie.sama@gmail.com>
2017-09-28 21:36:41 +0800

    init

---
+++ RYPoetry/Pods/Target Support Files/Pods-RYPoetry/Pods-RYPoetry-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_RYPoetry : NSObject
+@end
+@implementation PodsDummy_Pods_RYPoetry
+@end
---
+++ RYPoetry/RYPoetry/Classes/Home/RPHomeController.swift
@@ -0,0 +1,31 @@
+//
+//  RPHomeController.swift
+//  RYPoetry
+//
+//  Created by 王荣庆 on 2017/9/28.
+//  Copyright © 2017年 RyukieSama. All rights reserved.
+//
+
+import UIKit
+
+class RPHomeController: UIViewController {
+
+    // MARK: - Life
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setupUI()
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    // MARK: - function
+    private func setupUI() {
+        view.backgroundColor = UIColor.blue
+        
+    }
+    
+}
