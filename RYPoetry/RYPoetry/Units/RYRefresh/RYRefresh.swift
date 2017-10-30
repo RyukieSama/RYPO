5024b0bcbdc3fc4c5fc557fce8f871e1c59c2d89
RyukieSama <ryukie.sama@gmail.com>
2017-10-30 14:09:30 +0800

    Kingfisher

---
+++ RYPoetry/RYPoetry/Units/RYRefresh/RYRefresh.swift
@@ -0,0 +1,20 @@
+//
+//  RYRefresh.swift
+//  RYPoetry
+//
+//  Created by 王荣庆 on 2017/10/24.
+//  Copyright © 2017年 RyukieSama. All rights reserved.
+//
+
+import UIKit
+
+extension UITableView {
+    typealias RYRefreshClosures = () -> ()
+    
+//    var headerClosures : RYRefreshClosures?
+//    var footerClosures : RYRefreshClosures?
+    
+    func ryHeaderRefresh() {
+        
+    }
+}
